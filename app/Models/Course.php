edee374763bd02febe0df066db3cdcd70f6ba85d
Ceogo <sydykov.rus@yandex.kz>
2025-03-05 10:10:29 +0500

    Minor Changes

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,4 +9,9 @@ class Course extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description', 'image', 'price', 'features', 'category'];
+
+    public function curriculum()
+    {
+        return $this->hasMany(Curriculum::class);
+    }
 }
